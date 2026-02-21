1d372d49c4d63fde6b0f2773a40d719c2b1749bb
Andi Tse <103150017+AndiJingzhiTse@users.noreply.github.com>
2026-02-21 17:34:32 -0500

    Fix edge cases

--- Unit1AssignmentPartB.java
+++ Unit1AssignmentPartB.java
@@ -4,58 +4,63 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Unit1AssignmentPartB {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(new File("input.txt"));
-		// open the file to scan the input
-		while (sc.hasNextLine()) {
-			String input = sc.nextLine();
-			input = decode(input);
-			// decoding by using the method "decode"
-			System.out.println(input);
-		}
-		sc.close();
 
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        // open the file to scan the input
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            input = decode(input);
+            // decoding by using the method "decode"
+            System.out.println(input);
+        }
+        sc.close();
+    }
 
-	public static String decode(String input) {
-		String output = "";
-		for (int i = 0; i < input.length(); i++) {
-			// scan the string char by char
-			if (input.charAt(i) == '%') {
-				// if the i character of the string is %, then the word behind % will be the hex
-				// which we need to decode
-				if(i+3 > input.length()) {
-					output = "Error:  input is invalid, hex must be a two digit";
-					return output;
-				}
-				// to see if it is legal
-				
-				String num = input.substring(i + 1, i + 3);
-				// take out the two letter string from the first char after % to the second char
-				// after the % to decode
-				char firstChar = num.charAt(0);
-				char secondChar = num.charAt(1);
-				if(firstChar > 'F' || secondChar > 'F') {
-					output = "Error:  input out of ASCII range";
-					return output;
-				}
-				// to see if is out of range
-				int dec = Integer.parseInt(num, 16);
-				// decode by using parseInt
-				if(dec < 32) {
-					output = "Error:  not printable character";
-					return output;
-				}
-				// to check if the output is printable
-				char converted = (char) dec;
-				output += converted;
-				i += 2;
-				// skip the hex in case the for loop put the same thing into the output twice
-			} else {
-				output += input.charAt(i);
-			}
-		}
-		return output;
-	}
+    public static String decode(String input) {
+        String output = "";
+        for (int i = 0; i < input.length(); i++) {
+            // scan the string char by char
+            if (input.charAt(i) == '%') {
+                // if the i character of the string is %, then the word behind % will be the hex
+                // which we need to decode
+                if (i + 3 > input.length()) {
+                    // if less than 2 chars are behind %
+                    output += input.substring(i);
+                    return output;
+                }
+                // to see if it is legal
+
+                String num = input.substring(i + 1, i + 3);
+                // take out the two letter string from the first char after % to the second char
+                // after the % to decode
+                char firstChar = num.charAt(0);
+                char secondChar = num.charAt(1);
+                if (firstChar > 'F' || secondChar > 'F') {
+                    output += '%' + num;
+                    i += 2;
+                    continue;
+                }
+                // to see if is out of range
+                int dec = Integer.parseInt(num, 16);
+                // decode by using parseInt
+
+                if (dec < 32) {
+					// if not printable char
+                    output += '%' + num;
+                    i += 2;
+                    continue;
+                }
+                // to check if the output is printable
+                char converted = (char) dec;
+                output += converted;
+                i += 2;
+                // skip the hex in case the for loop put the same thing into the output twice
+            } else {
+                output += input.charAt(i);
+            }
+        }
+        return output;
+    }
 
 }
--- Unit1AssignmentPartC.java
+++ Unit1AssignmentPartC.java
@@ -1,32 +1,73 @@
 package assignment1;
+
 import java.io.*;
 import java.util.Scanner;
-public class Unit1AssignmentPartC {
-	public static void main(String []args)throws IOException {
-		Scanner sc = new Scanner(new File("input2.txt"));
-		int caseNum = sc.nextInt();
-		for(int i = 1; i <= caseNum; i++) {
-			int[] input = new int[sc.nextInt()];
-			for(int j = 0; j < input.length; j++) {
-				input[j] = sc.nextInt();
-			}
-			int max = findSum(input);
-			System.out.println("Case #" + i + ": " + max);
-		}
-		sc.close();
-	}
-	public static int findSum(int[] numArray) {
-		int max = Integer.MIN_VALUE;
-		for(int i = 0; i < numArray.length; i++) {
-			int sum = 0;
-			for(int j = i; j < numArray.length; j++) {
-				sum += numArray[j];
-				if(max < sum) {
-					max = sum;
-				}
-			}
-		}
-		return max;
-	}
 
+public class Unit1AssignmentPartC {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("input2.txt"));
+
+        int caseNum = isValidCount(sc.next());
+        if (caseNum == -1) {
+            sc.close();
+            return;
+        }
+
+        test_cases:
+        for (int i = 1; i <= caseNum; i++) {
+            int arrLength = isValidCount(sc.next());
+            if (arrLength == -1) {
+                if (i < caseNum) {
+                    sc.nextLine();
+                    sc.nextLine();
+                }
+                continue;
+            }
+            int[] input = new int[arrLength];
+
+            for (int j = 0; j < input.length; j++) {
+                try {
+                    int num = Integer.parseInt(sc.next());
+                    input[j] = num;
+                } catch (NumberFormatException e) {
+                    if (i < caseNum) {
+                        sc.nextLine();
+                        sc.nextLine();
+                    }
+                    continue test_cases;
+                }
+            }
+            int max = findSum(input);
+            System.out.println("Case #" + i + ": " + max);
+        }
+        sc.close();
+    }
+
+    public static int findSum(int[] numArray) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < numArray.length; i++) {
+            int sum = 0;
+            for (int j = i; j < numArray.length; j++) {
+                sum += numArray[j];
+                if (max < sum) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int isValidCount(String str) {
+        try {
+            int count = Integer.parseInt(str);
+            if (count > 0) {
+                return count;
+            } else {
+                return -1;
+            }
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
 }
